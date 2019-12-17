af1f1f8c3cb97bb98b8d0b986a8c0ae06dd07f07
baizheng <43673946@qq.com>
2019-12-17 21:20:29 +0800

    m

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Login from "../views/Login/index";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    redirect: "/login"
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
